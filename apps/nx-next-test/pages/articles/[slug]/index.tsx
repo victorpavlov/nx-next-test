2daab98fe14609b253fe6f34d6cefe87719e6e5e
Victor Pavlov <vitekpavlov@gmail.com>
2022-06-27 19:28:26 +0300

    Add markdown parse.

--- apps/nx-next-test/pages/articles/[slug]/index.tsx
+++ apps/nx-next-test/pages/articles/[slug]/index.tsx
@@ -1,43 +1,71 @@
-
+import fs from 'fs';
+import { join } from 'path';
 import { GetStaticPaths, GetStaticProps } from 'next';
 import { ParsedUrlQuery } from 'querystring';
+import {
+  getParsedFileContentBySlug,
+  MarkdownRenderingResult,
+  renderMarkdown,
+} from '@nx-test/markdown';
+// import styles from './articles.module.css';
 
 /* eslint-disable-next-line */
 export interface ArticleProps extends ParsedUrlQuery {
   slug: string;
 }
 
-export function Slug(props: ArticleProps) {
+const POSTS_PATH = join(process.cwd(), '_articles');
+
+export function Article({ frontMatter, html }) {
   return (
-    <div>
-      <h1>Welcome to { props.slug }!</h1>
+    <div className="md:container md:mx-auto">
+      <article>
+        <h1 className="text-3xl font-bold hover:text-gray-700 pb-4">
+          {frontMatter.title}
+        </h1>
+        <div>by {frontMatter.author.name}</div>
+        <hr />
+
+        <main dangerouslySetInnerHTML={{ __html: html }} />
+      </article>
     </div>
   );
 }
 
-export const getStaticProps: GetStaticProps<ArticleProps> = async ({
+export const getStaticProps: GetStaticProps<MarkdownRenderingResult> = async ({
   params,
 }: {
   params: ArticleProps;
 }) => {
+  // read markdown file into content and frontmatter
+  const articleMarkdownContent = getParsedFileContentBySlug(
+    params.slug,
+    POSTS_PATH
+  );
+  const renderedHTML = await renderMarkdown(articleMarkdownContent.content);
+
   return {
     props: {
-      slug: params.slug,
+      frontMatter: articleMarkdownContent.frontMatter,
+      html: renderedHTML,
     },
   };
 };
 
 export const getStaticPaths: GetStaticPaths<ArticleProps> = async () => {
+  const paths = fs
+    .readdirSync(POSTS_PATH)
+    // Remove file extensions for page paths
+    .map((path) => path.replace(/\.mdx?$/, ''))
+    // Map the path into the static paths object required by Next.js
+    .map((slug) => ({ params: { slug } }));
+
+  console.log(paths);
+
   return {
-    paths: [1, 2, 3, 4].map((idx) => {
-      return {
-        params: {
-          slug: `page${idx}`,
-        },
-      };
-    }),
+    paths,
     fallback: false,
   };
 };
 
-export default Slug;
+export default Article;
