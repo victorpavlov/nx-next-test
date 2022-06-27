ed45f18c0c253ececab2f6f0015b9be087b8d38a
Victor Pavlov <vitekpavlov@gmail.com>
2022-06-27 16:26:19 +0300

    Add Tailwind to project.

---
+++ apps/nx-next-test/tailwind.config.js
@@ -0,0 +1,18 @@
+const { join } = require('path');
+
+const { createGlobPatternsForDependencies } = require('@nrwl/next/tailwind');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  presets: [require('../../tailwind-workspace-preset.js')],
+  content: [
+    join(__dirname, 'pages/**/*.{js,ts,jsx,tsx}'),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
